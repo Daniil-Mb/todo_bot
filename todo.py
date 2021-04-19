ec22adc33fdfb2cea84565649d334ab8b4a0a0a8
Daniil1564 <>
2021-04-19 16:04:58 +0000

    добавили основную логику

---
+++ todo.py
@@ -0,0 +1,25 @@
+HELP = """
+HELP - помощь
+add  - добавить
+show  - показать
+done   - убрать
+exit    - выкл
+"""
+
+todo = {} 
+print("Введите команду или HELP, для вывода доступных команд")
+
+while True:
+  userUnswer = input()
+
+  if userUnswer == "add":
+    print("Работает ")
+  elif userUnswer == "help":
+    print(HELP) 
+  elif userUnswer == "show":
+    print("Работает ") 
+  elif userUnswer == "exit":
+    break
+  elif userUnswer == "done":
+    print("Работает ") 
+
